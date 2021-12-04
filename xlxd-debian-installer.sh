b0fb1d06969d75c2b103e0174545e316e743beba
M0VUB <76499782+ShaYmez@users.noreply.github.com>
2021-12-04 22:42:26 +0000

    Update xlxd-debian-installer.sh
    More libs

--- xlxd-debian-installer.sh
+++ xlxd-debian-installer.sh
@@ -24,7 +24,7 @@ WEBDIR=/var/www/xlxd
 XLXINSTDIR=/root/reflector-install-files/xlxd
 DEP="git build-essential g++ apache2 php libapache2-mod-php php7.0-mbstring"
 DEP2="git build-essential g++ apache2 php libapache2-mod-php php7.3-mbstring"
-DEP3="build-essential g++ apache2 php libapache2-mod-php"
+DEP3="build-essential g++ apache2 php libapache2-mod-php php-curl php-json php-cgi"
 VERSION=$(sed 's/\..*//' /etc/debian_version)
 clear
 echo ""
